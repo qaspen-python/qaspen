5019983b838c99bcc5dd0f3ef02d76346775117e
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2023-11-24 20:21:00 +0100

    Continue adding tests

--- tests/test_table/test_base_table.py
+++ tests/test_table/test_base_table.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from qaspen.aggregate_functions.general_purpose import Count
+from qaspen.fields.primitive import VarCharField
+from qaspen.table.base_table import BaseTable
 from tests.test_table.conftest import InheritanceBetaTable
 
 
@@ -34,3 +36,16 @@ def test_base_table_select() -> None:
         str(select_stmt_with_fields_and_agg.querystring())
         == "SELECT btable.field2, COUNT(btable.field1) FROM public.btable"
     )
+
+
+def test_base_table_all_fields() -> None:
+    """Test `all_fields` method."""
+
+    class FotTestInheritanceBetaTable(BaseTable):
+        field1: VarCharField = VarCharField()
+        field2: VarCharField = VarCharField()
+
+    assert FotTestInheritanceBetaTable.all_fields() == [
+        FotTestInheritanceBetaTable.field1,
+        FotTestInheritanceBetaTable.field2,
+    ]
