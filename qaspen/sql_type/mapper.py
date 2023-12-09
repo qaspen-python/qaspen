094c05a014612170d4ba9dff04301c7104211870
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2023-12-09 16:28:59 +0100

    Refactored agg functions

---
+++ qaspen/sql_type/mapper.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from datetime import date, datetime, time
+from typing import TYPE_CHECKING, Any
+
+from qaspen.sql_type.primitive_types import (
+    BigInt,
+    Date,
+    Decimal,
+    Time,
+    Timestamp,
+    VarChar,
+)
+
+if TYPE_CHECKING:
+    from qaspen.sql_type.base import SQLType
+
+
+def map_python_type_to_sql(  # noqa: PLR0911
+    for_match_value: Any,
+) -> type[SQLType] | None:
+    """Map python type to SQL type.
+
+    ### Returns:
+    subclass of `SQLType`.
+    """
+    if isinstance(for_match_value, str):
+        return VarChar
+    if isinstance(for_match_value, int):
+        return BigInt
+    if isinstance(for_match_value, float):
+        return Decimal
+    if isinstance(for_match_value, date):
+        return Date
+    if isinstance(for_match_value, time):
+        return Time
+    if isinstance(for_match_value, datetime):
+        return Timestamp
+
+    return None
