0f01b92a7876e2164d7c4d248ed1c8932fe86882
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2024-01-10 20:43:31 +0100

    Continue adding DeleteStatement

--- qaspen/statements/delete_statement.py
+++ qaspen/statements/delete_statement.py
@@ -1,8 +1,19 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Final, Generic
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Final,
+    Generator,
+    Generic,
+    List,
+    Optional,
+)
 
+from qaspen.exceptions import FieldDeclarationError
 from qaspen.qaspen_types import FromTable
+from qaspen.querystring.querystring import QueryString
 from qaspen.statements.base import Executable
 from qaspen.statements.combinable_statements.filter_statement import (
     FilterStatement,
@@ -12,6 +23,8 @@ from qaspen.statements.statement import BaseStatement
 if TYPE_CHECKING:
     from typing_extensions import Self
 
+    from qaspen.abc.db_engine import BaseEngine
+    from qaspen.abc.db_transaction import BaseTransaction
     from qaspen.fields.base import Field
     from qaspen.statements.combinable_statements.combinations import (
         CombinableExpression,
@@ -20,7 +33,7 @@ if TYPE_CHECKING:
 
 class DeleteStatement(
     BaseStatement,
-    Executable[None],
+    Executable[Optional[List[Dict[str, Any]]]],
     Generic[FromTable],
 ):
     """Statement for DELETE queries."""
@@ -34,6 +47,106 @@ class DeleteStatement(
         self._is_where_used: bool = False
         self._force: bool = False
         self._returning: tuple[Field[Any], ...] | None = None
+
+    def __await__(
+        self: Self,
+    ) -> Generator[None, None, list[dict[str, Any]] | None]:
+        """DeleteStatement can be awaited.
+
+        ### Returns:
+        result from `execute` method.
+        """
+        engine: Final = self._from_table._table_meta.database_engine
+        if not engine:
+            engine_err_msg: Final = "There is no database engine."
+            raise AttributeError(engine_err_msg)
+
+        return self.execute(engine=engine).__await__()
+
+    async def execute(
+        self: Self,
+        engine: BaseEngine[Any, Any, Any],
+    ) -> list[dict[str, Any]] | None:
+        """Execute DELETE statement.
+
+        This is manual execution.
+        You can pass specific engine.
+
+        ### Parameters
+        - `engine`: subclass of BaseEngine.
+
+        ### Returns
+        `SelectStatementResult`
+        """
+        querystring, qs_parameters = self.querystring().build()
+        raw_query_result: list[dict[str, Any]] | None = await engine.execute(
+            querystring=querystring,
+            querystring_parameters=qs_parameters,
+            fetch_results=bool(self._returning),
+        )
+
+        return raw_query_result
+
+    async def transaction_execute(
+        self: Self,
+        transaction: BaseTransaction[Any, Any],
+    ) -> list[dict[str, Any]] | None:
+        """Execute DELETE statement inside a transaction context.
+
+        This is manual execution.
+        You can pass specific transaction.
+        IMPORTANT: To commit the changes, with this way of execution,
+        it's necessary to manually call `await transaction.commit()`.
+
+        ### Parameters:
+        - `transaction`: running transaction.
+        database response or not.
+
+        ### Returns
+        `InsertStatement`
+        """
+        querystring, qs_parameters = self.querystring().build()
+        raw_query_result: list[
+            dict[str, Any]
+        ] | None = await transaction.execute(
+            querystring=querystring,
+            querystring_parameters=qs_parameters,
+            fetch_results=bool(self._returning),
+        )
+
+        return raw_query_result
+
+    def querystring(self: Self) -> QueryString:
+        """Build final querystring for UpdateStatement.
+
+        Firstly it checks are there where clause or force flag,
+        if not than raise an error.
+
+        After it builds querystring, adds where clause
+        and returning if necessary.
+
+        ### Returns:
+        `QueryString`.
+        """
+        if not self._is_where_used and not self._force:
+            no_where_clause_error = (
+                "You can't make DELETE queries without WHERE clause. "
+                "You can allow it with `.force()` method.",
+            )
+            raise FieldDeclarationError(no_where_clause_error)
+
+        querystring = QueryString(
+            self._from_table.table_name(),
+            sql_template=f"DELETE FROM {QueryString.arg_ph()}",
+        )
+
+        if self._is_where_used:
+            querystring += self._filter_statement.querystring()
+
+        if self._returning:
+            querystring += self._returning_query()
+
+        return querystring
 
     def where(
         self: Self,
@@ -98,3 +211,16 @@ class DeleteStatement(
         """
         self._force = False
         return self
+
+    def _returning_query(self: Self) -> QueryString:
+        if not self._returning:  # pragma: no cover
+            return QueryString.empty()
+
+        returning_template: Final = ", ".join(
+            ["{}"] * len(self._returning),
+        )
+
+        return QueryString(
+            *self._returning,
+            sql_template="RETURNING " + returning_template,
+        )
